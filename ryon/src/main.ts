01017e7120560cf0a453034682797a5c040ee73a
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-20 03:29:43 +0100

    ok

--- ryon/src/main.ts
+++ ryon/src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import './index.css';
 import router from './router'
+import App from './App.vue'
 
 const app = createApp(App)
 
